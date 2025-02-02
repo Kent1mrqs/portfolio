0a485a73146691c035a47674af411db6d47e6189
Quentin <marquesquentin@icloud.com>
2025-02-02 19:01:11 +0100

    reset readme

--- src/content/profileData.ts
+++ src/content/profileData.ts
@@ -1,29 +1,35 @@
 export const PROFILE = {
   site: {
     SEO: {
-      //     more complicated description for SEO
       title: "Quentin",
-      description:
-        "",
+      description: "Quentin Marques"
     },
   },
   timezone: "Europe/Paris",
   language: "en-AU",
   firstName: "Quentin",
   name: "Quentin Marques",
-  headLine: "headLine️",
+  headLine: "Welcome to my dashboard ! ",
   headLine2:
     "headLine2",
   website: "https://www.quentinmarques.com",
   repo: "repo",
   avatarUrl: "https://www.quentinmarques.com/avatar.webp",
+  presentation: "Welcome to my dashboard.",
   shortIntros: [
+    "🏠 Home Automation Passionate",
+    "🌐 Self-hosting Enthusiast",
+    "",
+    "🐱 Cat Lover | 🎲 Board Game Player"
   ],
   words: [""],
   skills: [
+    "Docker",
+    "Ansible"
   ],
   links: {
     github: "https://github.com/Kent1mrqs",
+    boardgamearena: "https://boardgamearena.com/player?id=92661770",
   },
   studies: [
   ],
